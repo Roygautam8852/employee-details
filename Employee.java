d55db017c48da5aa5d946a8ffd54faac6586a2b4
Gautam k <raygautam8252@gmail.com>
2025-11-18 13:54:38 +0530

    employee department and employee salary added

--- Employee.java
+++ Employee.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 class Employee {
     int id;
     String name;
+    String department;
+    float salary;
  
-    Employee(int id, String name) {
+    Employee(int id, String name,String department,float salary) {
         this.id = id;
         this.name = name;
+	this.department=department;
+	this.salary=salary;
     }
 
     
@@ -13,6 +17,8 @@ class Employee {
         System.out.println("\n--- Employee Details ---");
         System.out.println("Employee ID    : " + id);
         System.out.println("Name           : " + name);
+	System.out.println("Department     :"  + department);
+	System.out.println("Salary         :"  + salary);
     }
 }
 
@@ -29,8 +35,13 @@ class EmployeeMain {
         System.out.print("Enter Employee Name: ");
         String name = sc.nextLine();
 
+	System.out.print("Enter Employee Department: ");
+	String department=sc.nextLine();
+	
+	System.out.print("Enter Employee Salary: ");
+	float salary=sc.nextFloat();
      
-        Employee emp = new Employee(id, name);
+        Employee emp = new Employee(id, name,department,salary);
 
         
         emp.display();
